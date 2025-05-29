65014d0cf7a919a311c827d4fdadfdf72fc265f0
Mohamed Atallah <mohamedatallah@MacBook-Air-Mohamed.local>
2025-05-29 16:14:04 +0300

    Changes

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -11,7 +11,32 @@ import ThemeKit
 struct DailyScrum: Identifiable {
     let id: UUID = UUID()
     var title: String
-    var attendees: [String]
+    var attendees: [Attendee]
     var lengthInMinutes: Int
+    var lengthInMinutesAsDouble: Double {
+        get {
+            Double(lengthInMinutes)
+        }
+        set {
+            lengthInMinutes = Int(newValue)
+        }
+    }
     var theme: Theme
+    
+    init(title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.title = title
+        self.attendees = attendees.map { Attendee(name: $0) }
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
+}
+
+extension DailyScrum {
+    struct Attendee: Identifiable {
+        let id: UUID = UUID()
+        var name: String
+    }
+    static var emptyScurm: DailyScrum {
+        DailyScrum(title: "", attendees: [], lengthInMinutes: 5, theme: .sky)
+    }
 }
